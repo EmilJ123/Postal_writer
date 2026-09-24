a178ba71b33845b47edd59fd9373ba2174ee4570
Sevxnop <nataliakrakowska6@gmail.com>
2026-09-24 13:11:20 +0200

    script

--- BA_Postal_Unity/Assets/Objectives/Questsystem/S_QuestManager.cs
+++ BA_Postal_Unity/Assets/Objectives/Questsystem/S_QuestManager.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
 
-public class S_QuestManager : MonoBehaviour
+[CreateAssetMenu(menuName = "Quests/Quest")]
+public class S_QuestManager : ScriptableObject
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public string questID;
+    public string questName;
+    public string description;
+    public List<QuestObjective> objectives;
+    
+    private void OnEnable()
     {
-        
+        if (string.IsNullOrEmpty(questID))
+        {
+            questID = questName + Guid.NewGuid().ToString();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [System.Serializable]
+    public class QuestObjective
     {
-        
+        public string objectiveID; //Match with item ID that you need to collect, enemy ID that you need to kill etc
+        public string description;
+        public ObjectiveType type;
+        public int requiredAmount;
+        public int currentAmount;
+
+        public bool IsCompleted => currentAmount >= requiredAmount;
     }
+    
+    public enum ObjectiveType { CollectItem, ReachLocation, TalkNPC, Custom}
+
+    [System.Serializable]
+    public class QuestProgress
+    {
+        public Quest quest;
+        public List<QuestObjective> objectives;
+        public QuestProgress(Quest quest)
+        {
+            this.quest = quest;
+            objectives = new List<QuestObjective>();
+
+            //Deep copy avoid modifying original
+            foreach(var obj in objectives)
+            {
+                objectives.Add(new QuestObjective
+                {
+                    objectiveID = obj.objectiveID,
+                    description = obj.description,
+                    type = obj.type,
+                    requiredAmount = obj.requiredAmount,
+                    currentAmount = 0
+                });
+            }
+
+        }
+
+        public bool IsCompleted => objectives.TrueForAll(o => o.IsCompleted);
+        public string QuestID => quest.questID;
+    }
+
 }
