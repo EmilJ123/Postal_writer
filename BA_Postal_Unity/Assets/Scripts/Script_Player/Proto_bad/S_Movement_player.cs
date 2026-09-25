4e2077c4b2cc422e470eb3a0fa3a2ebb8a0402d8
EmilJ123 <ewaltoftolsen@gmail.com>
2026-09-25 14:04:19 +0200

    it fucking work

--- BA_Postal_Unity/Assets/Scripts/Script_Player/Proto_bad/S_Movement_player.cs
+++ BA_Postal_Unity/Assets/Scripts/Script_Player/Proto_bad/S_Movement_player.cs
@@ -1,7 +1,7 @@
-using System;
+
 using UnityEngine;
 using UnityEngine. InputSystem;
-using System.Collections.Generic;
+
 public class S_Movement_player : MonoBehaviour
 {
     public InputActionAsset actionAsset;
