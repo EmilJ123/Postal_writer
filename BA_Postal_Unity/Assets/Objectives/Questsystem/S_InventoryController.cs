19cf0016413288a229aa0e42088129ea24f654c4
Sevxnop <nataliakrakowska6@gmail.com>
2026-09-25 11:26:04 +0200

    inventory + save

--- BA_Postal_Unity/Assets/Objectives/Questsystem/S_InventoryController.cs
+++ BA_Postal_Unity/Assets/Objectives/Questsystem/S_InventoryController.cs
@@ -1,23 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class S_InventoryController : MonoBehaviour
 {
+    private S_ItemDictionary itemDictionary;
     public GameObject inventoryPanel;
     public GameObject slotPrefab;
    public int slotCount; // 
    public GameObject[] itemPrefabs;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [System.Obsolete]
     void Start()
     {
-        for (int i = 0; i < slotCount; i++)
+        itemDictionary = FindObjectOfType<S_ItemDictionary>();
+
+        //for (int i = 0; i < slotCount; i++)
+        //{
+            //S_Slot slot = Instantiate(slotPrefab, inventoryPanel.transform).GetComponent<S_Slot>();
+            //if(i < itemPrefabs.Length)
+            //{
+              //  GameObject item = Instantiate(itemPrefabs[i], slot.transform);
+                //item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero; //Puts the item in the middle of the slot
+                //slot.currentItem = item;
+            //}
+        //}
+    }
+
+    public List<S_InventorySaveData> GetInventoryItems()
+    {
+        List<S_InventorySaveData> invData = new List<S_InventorySaveData>();
+        foreach(Transform slotTransform in inventoryPanel.transform)
         {
-            S_Slot slot = Instantiate(slotPrefab, inventoryPanel.transform).GetComponent<S_Slot>();
-            if(i < itemPrefabs.Length)
+            S_Slot slot = slotTransform.GetComponent<S_Slot>();
+            if(slot.currentItem != null)
             {
-                GameObject item = Instantiate(itemPrefabs[i], slot.transform);
-                item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero; //Puts the item in the middle of the slot
-                slot.currentItem = item;
+                S_Item item = slot.currentItem.GetComponent<S_Item>();
+                invData.Add(new S_InventorySaveData { itemID = item.ID, slotIndex = slotTransform.GetSiblingIndex() });
             }
         }
+        return invData;
     }
+
+    public void SetInventoryItems(List<S_InventorySaveData> inventorySaveData)
+    {
+        foreach(Transform child in inventoryPanel.transform)
+        {
+           Destroy(child.gameObject); 
+        }
+
+        for(int i = 0; i < slotCount; i++)
+        {
+            Instantiate(slotPrefab, inventoryPanel.transform);
+        }
+
+        foreach(S_InventorySaveData data in inventorySaveData)
+        {
+            if(data.slotIndex < slotCount)
+            {
+                S_Slot slot = inventoryPanel.transform.GetChild(data.slotIndex).GetComponent<S_Slot>();
+                GameObject itemPrefab = itemDictionary.GetItemPrefab(data.itemID);
+                if(itemPrefab != null)
+                {
+                    GameObject item = Instantiate(itemPrefab, slot.transform);
+                    item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero; 
+                    slot.currentItem = item;
+                }
+            
+            }
+        }
+
+    }
+
 }
---
+++ BA_Postal_Unity/Assets/Objectives/Questsystem/S_InventorySaveData.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class S_InventorySaveData
+
+{
+    public int itemID;
+    public int slotIndex; //The index of the slot where the item is placed
+}
